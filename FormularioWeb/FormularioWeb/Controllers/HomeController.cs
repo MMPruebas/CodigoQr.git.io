9bfecb2e8e3b3eece753da63aff9235aa8add209
GPJD <74803693+GPJD@users.noreply.github.com>
2021-07-24 12:08:27 -0500

    new
    Proyecto prueba

---
+++ FormularioWeb/FormularioWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa;
+using ZXing;
+using System.Configuration;
+using ZXing.Common;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FormularioWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string folio = "CodigoQr";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult qr()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult qr(string nombreDoc)
+        {
+            return new ActionAsPdf("qr")
+            { FileName = nombreDoc + ".pdf" };
+            
+        }
+        [HttpPost]
+        public ActionResult Index(string url)
+        {
+
+            string ubicacion = ConfigurationManager.AppSettings["Ubicacion"].ToString();
+            var write = new BarcodeWriter()
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions()
+                {
+                    Height = 300,
+                    Width = 300,
+                    Margin = 1,
+                },
+            };
+
+            Bitmap bitmap = write.Write(url);
+            bitmap.Save(String.Format(ubicacion+"{0}.png", folio), ImageFormat.Png);
+            return View();
+        }
+        
+    }
+}
---
+++ FormularioWeb/FormularioWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FormularioWeb.MvcApplication" Language="C#" %>
